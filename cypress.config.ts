0667e41ae4d3930748505fd69aeb8ab5fba9f747
MouhamadouMoustaphaDia <diatapha00@gmail.com>
2025-12-09 13:52:07 +0100

    Add video and HTML reports generation with Mochawesome

--- cypress.config.ts
+++ cypress.config.ts
@@ -8,5 +8,16 @@ export default defineConfig({
     baseUrl: "http://automationexercise.com",
     supportFile: "cypress/support/e2e.ts",
     specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+    video: true,
+    screenshotOnRunFailure: true,
+    screenshotsFolder: "cypress/screenshots",
+    videosFolder: "cypress/videos",
+    reporter: "mochawesome",
+    reporterOptions: {
+      reportDir: "cypress/reports",
+      overwrite: false,
+      html: true,
+      json: true,
+    },
   },
 });
